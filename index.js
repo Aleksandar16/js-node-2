ad26004868c176f4a0149dc9dc24a8f2844f30fc
Aleksandar <a.milenkovic321@gmail.com>
2024-10-11 12:31:56 +0200

    code fix

--- index.js
+++ index.js
@@ -24,47 +24,46 @@ async function saveAsJson(data) {
 }
 
 async function saveAsCSV(data) {
-    try {
-      const csv = await converter.json2csv(data);
-      fs.writeFile("data.csv", csv, "utf-8", (err) => {
-        if (err) console.log(err);
-        else console.log("Data saved");
-      });
-      console.log('Les données ont été récupérées et enregistrées dans le fichier data.csv.');
-    } catch (error) {
-      console.error('Erreur lors de la récupération des données :', error);
-      throw error;
-    }
+  try {
+    const csv = await converter.json2csv(data);
+    fs.writeFile("data.csv", csv, "utf-8", (err) => {
+      if (err) console.log(err);
+    });
+    console.log('Les données ont été récupérées et enregistrées dans le fichier data.csv.');
+  } catch (error) {
+    console.error('Erreur lors de la récupération des données :', error);
+    throw error;
   }
+}
 
-  async function saveAsExcel(data) {
-    let i = 1;
-    try {
-      var wb = new xl.Workbook();
-      var ws = wb.addWorksheet('Sheet 1');
-      var style = wb.createStyle({
-        font: {
-          color: '#FF0800',
-          size: 12,
-        },
-        numberFormat: '$#,##0.00; ($#,##0.00); -',
-      });
-      data.forEach((d) => {
-        ws.cell(1, i)
-          .number(d.id)
-          .style(style);
-        ws.cell(2, i)
-          .string(d.first_name)
-          .style(style);
-        i++;
-      })
-      wb.write('data.xlsx');
-      console.log('Les données ont été récupérées et enregistrées dans le fichier data.xlsx.');
-    } catch (error) {
-      console.error('Erreur lors de la récupération des données :', error);
-      throw error;
-    }
+async function saveAsExcel(data) {
+  let i = 1;
+  try {
+    var wb = new xl.Workbook();
+    var ws = wb.addWorksheet('Sheet 1');
+    var style = wb.createStyle({
+      font: {
+        color: '#FF0800',
+        size: 12,
+      },
+      numberFormat: '$#,##0.00; ($#,##0.00); -',
+    });
+    data.forEach((d) => {
+      ws.cell(1, i)
+        .number(d.id)
+        .style(style);
+      ws.cell(2, i)
+        .string(d.first_name)
+        .style(style);
+      i++;
+    })
+    wb.write('data.xlsx');
+    console.log('Les données ont été récupérées et enregistrées dans le fichier data.xlsx.');
+  } catch (error) {
+    console.error('Erreur lors de la récupération des données :', error);
+    throw error;
   }
+}
 
 async function main() {
   const args = process.argv.slice(2);
@@ -73,27 +72,27 @@ async function main() {
     const data = await fetchData();
 
     switch(format){
-        case "json":
-            saveAsJson(data);
-            break;
-        case "csv":
-            data.forEach(d => {
-                delete d.employment
-                delete d.address;
-                delete d.credit_card;
-                delete d.subscription;
-            });
-            saveAsCSV(data);
-            break;
-        case "excel":
-            data.forEach(d => {
-                delete d.employment
-                delete d.address;
-                delete d.credit_card;
-                delete d.subscription;
-            });
-            saveAsExcel(data);
-            break;
+      case "json":
+        saveAsJson(data);
+        break;
+      case "csv":
+        data.forEach(d => {
+          delete d.employment
+          delete d.address;
+          delete d.credit_card;
+          delete d.subscription;
+        });
+        saveAsCSV(data);
+        break;
+      case "excel":
+        data.forEach(d => {
+          delete d.employment
+          delete d.address;
+          delete d.credit_card;
+          delete d.subscription;
+        });
+        saveAsExcel(data);
+        break;
     }
   } catch (error) {
     console.error('Erreur lors de la récupération des données :', error);
